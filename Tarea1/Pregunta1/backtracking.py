9d457f74ed5246466a59a697001ef00f94f12b10
jaimeRodriguezg <jaime.rodriguezg@sansano.usm.cl>
2020-11-10 02:07:49 -0300

    Pregunta 1 lista

--- Tarea1/Pregunta1/backtracking.py
+++ Tarea1/Pregunta1/backtracking.py
@@ -1,23 +1,14 @@
-
-t=6
-#t=8
-#t=800
-n=8
-#n=6
-#n=12
-lista = [6,4,3,3,3,2,2,2]
-#lista = [8,6,4,4,2,2]
-#lista = [100,100,100,100,100,100,50,50,50,50,50,50]
 lista_aux = []
 lista_soluciones = []
 suma = 0
-
-
+listaNueva= []
+    
+#funcion que verifica los resultados de la sumarecursiva usando backtracking
 def sumaRecursiva(t,n,lista, lista_aux, suma):
+    lista = lista [:]
     lista_aux = lista_aux[:] #hacemos una copia de la lista_aux, ya que las listas son mutables y se pasa la referencia 
     if suma == t:
-        lista_aux2 = lista_aux
-        lista_soluciones.append(lista_aux2) #agregamos la solucion a la lista de soluciones 
+        lista_soluciones.append(lista_aux) #agregamos la solucion a la lista de soluciones 
         return
     else:
         for pos in range(n):
@@ -25,12 +16,9 @@ def sumaRecursiva(t,n,lista, lista_aux, suma):
             suma = sum(lista_aux) # generamos la suma acumulativa de la lista auxiliar
             if suma == t or suma < t:
                 sumaRecursiva(t,len(lista[pos+1:]),lista[pos+1:],lista_aux,suma)
-                lista_aux.pop() #eliminamos el último elemento para seguir iterando
-            else:
-                lista_aux.pop()
+            lista_aux.pop()#eliminamos el último elemento para seguir iterando
         return
 
-sumaRecursiva(t,n,lista, lista_aux, suma)
 
 #funcion que elimina los elementos repetidos de la lista de soluciones
 def elementosRepetidos(lista):
@@ -61,9 +49,31 @@ def imprimirResultado(listaResultado, t):
     return 
 
 
+archivo = open("data.txt", 'r')
+lineas = archivo.readlines()
+print (lineas) 
 
-
-listaFinal = elementosRepetidos(lista_soluciones)
-imprimirResultado(listaFinal,t)
-
-
+for linea in lineas:
+    lista = []
+    print(f"la linea es {linea}")
+    string= ""
+    for index,elemento in enumerate(linea):
+        if elemento == " ":
+            listaNueva.append(string)
+            string = ""
+        else:
+            string += elemento
+            if index == len(linea) - 2:
+                listaNueva.append(string)
+    for index, elemento in enumerate(listaNueva):
+        if elemento != " " and elemento != "\n":
+            lista.append(int(elemento))
+    listaNueva = []
+    t = lista[0]
+    n = lista[1]
+    lista = lista[2:]
+    sumaRecursiva(t,n,lista, lista_aux, suma)
+    listaFinal = elementosRepetidos(lista_soluciones)
+    lista_soluciones = []
+    imprimirResultado(listaFinal,t)
+    listaFinal = []
