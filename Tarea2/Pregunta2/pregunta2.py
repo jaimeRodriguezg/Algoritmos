967a045322d72cb95c1f0842f51712550c1bb88f
valeriamirandam <58197549+valeriamirandam@users.noreply.github.com>
2020-12-15 12:57:14 -0300

    Add files via upload

---
+++ Tarea2/Pregunta2/pregunta2.py
@@ -0,0 +1,34 @@
+def suma_maximos_subarreglos(arreglo,resultado):
+    if(len(arreglo)==1):
+        return arreglo[0]
+    else:
+        inicio=0
+        fin=len(arreglo)-1
+        pos_max=arreglo.index(max(arreglo))
+        resultado = resultado + arreglo[pos_max]*(fin-pos_max+1)*(pos_max-inicio+1)
+        if pos_max==inicio:
+            resultado= resultado + suma_maximos_subarreglos(arreglo[pos_max+1:],0)
+        elif pos_max==fin:
+            resultado= resultado + suma_maximos_subarreglos(arreglo[:pos_max],0)
+        else:
+            resultado = resultado + suma_maximos_subarreglos(arreglo[:pos_max],0)+suma_maximos_subarreglos(arreglo[pos_max+1:],0)
+        return resultado
+def suma_minimos_subarreglos(arreglo,resultado):
+    if(len(arreglo)==1):
+        return arreglo[0]
+    else:
+        inicio=0
+        fin=len(arreglo)-1
+        pos_min=arreglo.index(min(arreglo))
+        resultado = resultado + arreglo[pos_min]*(fin-pos_min+1)*(pos_min-inicio+1)
+        if pos_min==inicio:
+            resultado= resultado + suma_minimos_subarreglos(arreglo[pos_min+1:],0)
+        elif pos_min==fin:
+            resultado= resultado + suma_minimos_subarreglos(arreglo[:pos_min],0)
+        else:
+            resultado = resultado + suma_minimos_subarreglos(arreglo[:pos_min],0)+suma_minimos_subarreglos(arreglo[pos_min+1:],0)
+        return resultado
+largo = int(input("").strip())
+listaString=input("").strip().split()
+lista = list(map(int, listaString)) 
+print(suma_maximos_subarreglos(lista,0)-suma_minimos_subarreglos(lista,0))
